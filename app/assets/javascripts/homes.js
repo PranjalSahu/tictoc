51a8ee5813308902b3ceede63f9b3bef87f00806
PRANJAL SAHU <pranjalsahu5@gmail.com>
2016-07-31 18:29:38 +0530

    validation

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,12 +1,9 @@
+var current = 0
+var size = 3
+var squaresize = 8
+var alreadydrawn = []
+
 window.onload = function() {
-	drawCircle("myCanvasa", "blue")
- //  var c   = document.getElementById("myCanvasa");
-	// var ctx = c.getContext("2d");
-	// ctx.beginPath();
-	// ctx.arc(80, 80, 50, 0, 2 * Math.PI);
-	// ctx.stroke();
-	// ctx.fillStyle = "blue";
-	// ctx.fill();
 };
 
 function drawCircle(divid, color) {
@@ -21,6 +18,15 @@ function drawCircle(divid, color) {
 
 $(function() {
     $("body").click(function(e) {
-    		drawCircle(e.target.id, "blue")
+    	  if (alreadydrawn.includes(e.target.id))
+    	  	return;
+    	  if (current%2 == 0) {
+    			drawCircle(e.target.id, "blue")
+    		}
+    		else {
+    			drawCircle(e.target.id, "red")
+    		}
+    		alreadydrawn.push(e.target.id)
+    		current = current + 1
     });
 })
